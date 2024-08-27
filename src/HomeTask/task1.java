7ba51a29c6a0a6251d34aafc36d9d5192a67d069
Egor Safuanov <91279825+Fazan4ik@users.noreply.github.com>
2024-08-27 14:05:22 +0300

    Update task1.java

--- src/HomeTask/task1.java
+++ src/HomeTask/task1.java
@@ -1,4 +1,10 @@
 package HomeTask;
 
 public class task1 {
+    public static void main(String[] args) {
+        System.out.println("\"Your time is limited,");
+        System.out.println("\tso don’t waste it");
+        System.out.println("\t\tliving someone else’s life\"");
+        System.out.println("\t\t\tSteve Jobs");
+    }
 }
